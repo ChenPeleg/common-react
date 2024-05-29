4659e0389a78695b754ce2f8e807d3896d37d4da
Chen Peleg <chenpeleg@gmail.com>
2024-05-29 11:35:02 +0300

    added from applinks site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+import colors from 'tailwindcss/colors';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'app-blue': colors.blue[600],
+        'app-purple': colors.indigo[500],
+      },
+    },
   },
   plugins: [],
-}
-
+};

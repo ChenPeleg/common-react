e23c0cad31f9349491b52d5b135baec77f2f09a6
Chen Peleg <chenpeleg@gmail.com>
2024-05-29 11:28:32 +0300

    starting to edit the site

---
+++ src/examples/Site.tsx
@@ -0,0 +1,9 @@
+export const Site = () => {
+    return <div className={'w-screen h-screen'} id={'site-root'}>
+        <main>
+            <h1 className={'text-2xl '}>
+                Common React components and hooks
+            </h1>
+        </main>
+    </div>;
+}

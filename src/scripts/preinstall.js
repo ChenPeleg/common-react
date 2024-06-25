4911879f08e7b85d116b0fc965dd6616375a5e02
Chen Peleg <chenpeleg@gmail.com>
2024-06-25 17:56:23 +0300

    added an uninstall file

--- src/scripts/preinstall.js
+++ src/scripts/preinstall.js
@@ -1,3 +1,9 @@
 
 
-console.info(`This package is not to be installed but rather used as a template for creating new packages. Please refer to the documentation for more information. Exiting...`)
+console.info(`This package is not to be installed but rather used as a template for creating new packages. /n
+Please refer to the documentation for more information:/n
+https://chenpeleg.github.io/common-react/#/about
+Exiting...`)
+
+// eslint-disable-next-line no-undef
+process.exit(0)

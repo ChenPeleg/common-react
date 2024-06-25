0b81eb9f53ce23bf931d5ae7e0c0dd3f569e8fe7
Chen Peleg <chenpeleg@gmail.com>
2024-06-25 10:58:10 +0300

    removed icon name from menu options

--- src/react-common/dropdown/MenuOption.ts
+++ src/react-common/dropdown/MenuOption.ts
@@ -1,9 +1,8 @@
 import { ReactNode } from 'react';
-import { IconName } from '../../styles/Icons.tsx';
 
 export interface MenuOption {
     label: string | ReactNode;
     id: string | number;
     data?: any;
-    icon?: string | IconName;
+    icon?: string
 }

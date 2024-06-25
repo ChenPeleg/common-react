cbfe7f2d6d7c132ea4812e3223e37d67475cd594
Chen Peleg <chenpeleg@gmail.com>
2024-06-25 09:59:11 +0300

    added the dialog modal 3

--- src/examples/ExampleMenu.tsx
+++ src/examples/ExampleMenu.tsx
@@ -1,40 +1,37 @@
-import { Popover } from '../react-common/popover/Popover.tsx';
 import { MenuOption } from '../react-common/dropdown/MenuOption.ts';
 import React from 'react';
 import { AppDropdown } from '../react-common/dropdown/AppDropDown.tsx';
 
 export const ExampleMenu = () => {
-    const [selectedOption, setSelectedOption] =
-        React.useState<MenuOption | null>(null);
-    const options = [
-        { label: 'Support', id: 'support' },
-        { label: 'Account Settings', id: 'account-settings' },
-        { label: 'Sign out', id: 'sign-out' },
-    ];
+    const [selectedOption, setSelectedOption] = React.useState<MenuOption | null>(null);
+    const options = [{
+        label: 'Support',
+        id: 'support',
+    }, {
+        label: 'Account Settings',
+        id: 'account-settings',
+    }, {
+        label: 'Sign out',
+        id: 'sign-out',
+    }];
     return (
-        <div
-            className={
-                'flex h-96 w-3/4 flex-col items-center  justify-between rounded-lg bg-gray-100 p-8 shadow-lg'
-            }
-        >
-            <Popover button={<button className={'globals-app-button'}>Show menu</button>}>
-                {(closePopover) => (
-                    <div className={'  bg-white p-5'}>
-                        <p className={'bg-white'}>
-                            Please input a correct task in the input field
-                        </p>
 
-                        <button onClick={closePopover}>Close</button>
-                    </div>
-                )}
-            </Popover>
-            <div>
-                <AppDropdown.Controlled
-                    options={options}
-                    selectedOption={selectedOption}
-                    setSelectedOption={setSelectedOption}
-                />
+        <div
+            className={'flex h-64 flex-col items-start justify-start  p-12 gap-4 '}
+        >
+            <p>
+                This dropdown is a simple example of a dropdown menu component,  based on the HTML
+                <a className={'text-app-blue'}
+                   href={'https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/popover'}> popover element.</a>
+            </p>
+            <div className={'z-50 w-64'}>
+                    <AppDropdown.Controlled
+                        config={{width: 200}}
+                        options={options}
+                        selectedOption={selectedOption}
+                        setSelectedOption={setSelectedOption}
+                    />
+
             </div>
-        </div>
-    );
+        </div>);
 };

1e10d51e5b9230328e09548fab5b7d9d65f3c652
Chen Peleg <chenpeleg@gmail.com>
2024-06-27 11:08:39 +0300

    updated assets files 3

--- src/react-common/scripts/renameFolder.js
+++ src/react-common/scripts/renameFolder.js
@@ -1,6 +1,7 @@
 //  rename folder
-import { renameSync, mkdirSync, existsSync, readDir,readFileSync,writeFileSync} from 'node:fs';
+import { renameSync, mkdirSync, existsSync,  readFileSync,writeFileSync ,readdirSync} from 'node:fs';
 import { resolve } from 'node:path';
+import { isDirectory } from './isDirectory.js';
 
 
 export const renameFolder = (oldPath, newPath, createPathIfNeeded = true) => {
@@ -19,9 +20,12 @@ export const copyFolderRecursively = async (source, target) => {
     if (!existsSync(resolve(target))) {
         mkdirSync(resolve(target), { recursive: true });
     }
-    const files = readDir(resolve(source));
-    for (const file of files) {
-        const current = readFileSync(resolve(source, file));
-        writeFileSync(resolve(target, file), current);
-    }
+    const is = isDirectory('temp/react-common2');
+
+  console.log('isDir', is)
+    // const files = readdirSync(resolve(source));
+    // for (const file of files) {
+    //     const current = readFileSync(resolve(source, file));
+    //     writeFileSync(resolve(target, file), current);
+    // }
 }

ccac0316e40e78048f506d0f47b4cee883cc928f
Chen Peleg <chenpeleg@gmail.com>
2024-06-30 15:57:48 +0300

    installing in the src folder

--- src/react-common/scripts/loadChangedAssets.js
+++ src/react-common/scripts/loadChangedAssets.js
@@ -3,75 +3,37 @@
 import { Readable } from 'node:stream';
 import { writeFile } from 'fs/promises';
 import { existsSync, mkdirSync } from 'node:fs';
-import { resolve } from 'node:path';
-import { platform } from 'node:os';
-import { exec } from 'child_process';
 
 
-
-
-const getLatestReleaseURL = async (owner, repo) => {
-  const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/releases/latest`);
-    if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
-    return await response.json();
-};
-
-
-
-const getLatestRelease  = (owner, repo) =>
-    getLatestReleaseURL(owner, repo)
-    .then(release => {
-        const asset = release.assets[0];
-        console.log('from' , asset.browser_download_url)
-        return fetch(asset.browser_download_url );
-    })
-    .catch(console.error);
-
-
-const execPromise = async (command, extraParams = {}) => {
-    return new Promise(function (resolve, reject) {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        exec(command, extraParams, (error, stdout, stderr) => {
-            if (error) {
-                reject(error);
-                return;
-            }
-            resolve(stdout.trim());
-        });
-    });
-};
-export const runUnZipper = async (
-    fileName = "workbook",
-    tempDir = "temp",
-    outDir = "out"
-) => {
-    if (platform() === "win32") {
-        const psCommand = `Expand-Archive -Path ${resolve(
-            tempDir)}/${fileName} -DestinationPath out`;
-         await execPromise(psCommand, {
-            cwd: tempDir,
-            shell: "powershell.exe",
-        });
-    } else {
-        const psCommand = `cd ${resolve(tempDir)} && zip -r ${resolve(
-            outDir
-        )}/${fileName}.zip ./*`;
-        const res = await execPromise(psCommand);
-        console.log(res);
-    }
-
-};
+import {
+    copyFolderRecursively, getLatestRelease,
+    getLatestReleaseData,
+    promptUserConsole,
+    runUnZipper,
+} from './getAssetsUtils.js';
 
 
 const updateChangedLibrary = async () => {
+    const releaseData = await getLatestReleaseData('ChenPeleg', 'common-react');
 
-    const response = await getLatestRelease('ChenPeleg', 'common-react');
+    // normalize the tag name to be a valid folder name
+    const versionName = releaseData.tag_name.replace(/[\\/:*?"<>|]/g, '_');
+    const response = await getLatestRelease(releaseData);
     const body = Readable.fromWeb(response.body);
-
     !existsSync('temp') && mkdirSync('temp');
-    !existsSync('temp/out') && mkdirSync('temp/out');
-    const filePath =  'temp/updatedlib.zip' ;
+    const filePath = 'temp/updatedlib.zip';
     await writeFile(filePath, body);
-    await runUnZipper('updatedlib.zip', 'temp', 'out');
+    await runUnZipper('updatedlib.zip', 'temp', 'temp');
+
+    if (versionName) {
+        // return;
+    }
+    const answer = await promptUserConsole( `"common-react" (${versionName}) was copied to
+     "temp" folder. Do you want to  move  it to the "src"  folder? ` + ' (y/n) ');
+    if (answer.trim().toLowerCase() === 'y'  ) {
+        !existsSync('src') && mkdirSync('src');
+        await copyFolderRecursively('temp/react-common', `src/react-common`);
+    }
 };
+console.log('Loading the latest version of "common-react" library')
 updateChangedLibrary().then();
